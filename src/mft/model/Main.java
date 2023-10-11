eaa9c946b2e8787ae33b43ab48d600e01d8e03b2
Messbah.A <Messbah.a@Gmail.com>
2023-10-11 18:37:32 +0330

    LAST UPDATE BY MESSBAH.A

--- src/mft/model/Main.java
+++ src/mft/model/Main.java
@@ -5,17 +5,6 @@ import mft.model.entity.Borrow;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BorrowDa borrowDa = new BorrowDa();
-        for (Borrow borrow : borrowDa.findAll()) {
-            System.out.println("Member : " + borrow.getMember());
-            System.out.println("Book : " + borrow.getBook());
-            System.out.println("Borrow Time : " + borrow.getBorrowTimeStamp());
-            if(borrow.getReturnTimeStamp() == null){
-                System.out.println("Not Returned");
-            }else{
-                System.out.println("Is Returned at " + borrow.getReturnTimeStamp());
-            }
-            System.out.println("------------------------------");
-        }
+
     }
 }
