3c53418474dfc4187d39fd370f08860d7d2ca301
Messbah.A <Messbah.A@Gmail.com>
2023-10-18 16:15:15 +0330

    JDBC updated messbah.a

--- src/mft/model/utils/Jdbc.java
+++ src/mft/model/utils/Jdbc.java
@@ -1,21 +1,25 @@
 package mft.model.utils;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Jdbc {
-    public Connection getConnection() throws Exception {
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        return DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521:xe",
-                "javase",
-                "java123"
-        );
+    private static BasicDataSource basicDataSource = new BasicDataSource();
+    public static Connection  getConnection() throws Exception {
+        basicDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        basicDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+        basicDataSource.setUsername("javase");
+        basicDataSource.setPassword("java123");
+        basicDataSource.setMinIdle(5);
+        basicDataSource.setMaxTotal(20);
+        return basicDataSource.getConnection();
     }
 
-    public int nextId(String sequenceName) throws Exception {
+    public static int nextId(String sequenceName) throws Exception {
         Connection connection = getConnection();
         PreparedStatement statement = connection.prepareStatement("SELECT "+ sequenceName+".NEXTVAL AS NEXT_ID FROM DUAL");
         ResultSet resultSet = statement.executeQuery();
