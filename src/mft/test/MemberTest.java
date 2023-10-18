a99dea41cd0f81479de7607cec9185130fd79923
royazargar <roya.zargar1992@gmail.com>
2023-10-18 17:34:20 +0330

    royazargar member updated

--- src/mft/test/MemberTest.java
+++ src/mft/test/MemberTest.java
@@ -5,10 +5,28 @@ import mft.model.entity.Member;
 
 public class MemberTest {
     public static void main(String[] args) throws Exception {
-        Member member = Member.builder().id(2).name("ahmad").family("messbah").build();
+//        Connection connection = Jdbc.getConnection();
+//        Member member = Member.builder().name("ahmad").family("messbah").build();
+//
+//        member.setId(Jdbc.nextId("MEMBER_SEQ"));
+//        PreparedStatement preparedStatement = connection.prepareStatement(
+//                "INSERT INTO MEMBER_TBL(ID,NAME,FAMILY) VALUES (?,?,?)"
+//        );
+//        preparedStatement.setInt(1, member.getId());
+//        preparedStatement.setString(2, member.getName());
+//        preparedStatement.setString(3, member.getFamily());
+//        preparedStatement.execute();
+//        preparedStatement.close();
+//        connection.close();
+
+
+        Member member = Member.builder().id(3).name("ahad").family("ahadvand").build();
         MemberDa memberDa = new MemberDa();
-        System.out.println(memberDa.save(member));
-        System.out.println(memberDa.findAll());
-        System.out.println(memberDa.findById(1));
+        //System.out.println(memberDa.save(member));
+        //memberDa.remove(1);
+//        System.out.println(memberDa.findByNameAndFamily("ahmad","messbah"));
+        System.out.println(memberDa.edit(member));
+//        System.out.println(memberDa.findAll());
+//        System.out.println(memberDa.findById(4));
     }
 }
