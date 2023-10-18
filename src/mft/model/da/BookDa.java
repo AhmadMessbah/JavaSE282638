fd80716ec64a0fb8c251678294258796e3ed8c32
Messbah.A <Messbah.A@Gmail.com>
2023-10-18 15:46:26 +0330

    BOOK_DA updated messbah.a

--- src/mft/model/da/BookDa.java
+++ src/mft/model/da/BookDa.java
@@ -1,4 +1,23 @@
 package mft.model.da;
 
+import mft.model.utils.Jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class BookDa {
+    private Connection connection;
+    private PreparedStatement statement;
+    private Jdbc jdbc = new Jdbc();
+
+    public BookDa() throws Exception {
+        connection = jdbc.getConnection();
+    }
+
+//    TODO : (save, edit, remove, findAll, findById)
+
+    public void close() throws Exception {
+        statement.close();
+        connection.close();
+    }
 }
