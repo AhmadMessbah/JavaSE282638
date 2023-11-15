9b248c7dc14ceed423be65ddeb6e0290679526ef
royazargar <roya.zargar1992@gmail.com>
2023-11-15 17:56:33 +0330

    member updated by royazargar

--- src/mft/view/MemberViewController.java
+++ src/mft/view/MemberViewController.java
@@ -9,74 +9,60 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import mft.controller.MemberController;
 import mft.model.entity.Member;
 import java.net.URL;
-
 import java.util.List;
+
 import java.util.ResourceBundle;
 
 public class MemberViewController implements Initializable {
     @FXML
-    private TextField idTxt,nameTxt,familyTxt;
+    private TextField idTxt, nameTxt, familyTxt;
     @FXML
-    private Button saveBtn,editeBtn,removeBtn;
+    private Button saveBtn, editBtn, removeBtn;
     @FXML
     private TableView<Member> table;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         resetForm();
+        table.setOnMouseClicked((event) -> {
+            Member member = table.getSelectionModel().getSelectedItem();
+            idTxt.setText(String.valueOf(member.getId()));
+            nameTxt.setText(String.valueOf(member.getName()));
+            familyTxt.setText(String.valueOf(member.getFamily()));
+        });
+
         saveBtn.setOnAction(event -> {
-            Member member=
-                    Member
-                            .builder()
-                            .id(Integer.parseInt(idTxt.getText()))
-                            .name(nameTxt.getText())
-                            .family(familyTxt.getText())
-                            .build();
-            Alert alert=new Alert(Alert.AlertType.INFORMATION,
-                    member.toString()+" Saved",
-                    ButtonType.OK);
+            Member member = Member.builder().id(Integer.parseInt(idTxt.getText())).name(nameTxt.getText()).family(familyTxt.getText()).build();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, member.toString() + " Saved", ButtonType.OK);
             alert.show();
             resetForm();
         });
-        editeBtn.setOnAction(event -> {
-            Member member=
-                    Member
-                            .builder()
-                            .id(Integer.parseInt(idTxt.getText()))
-                            .name(nameTxt.getText())
-                            .family(familyTxt.getText())
-                            .build();
-            Alert alert=new Alert(Alert.AlertType.INFORMATION,
-                    member.toString()+" Edited",
-                    ButtonType.OK);
+        editBtn.setOnAction(event -> {
+            Member member = Member.builder().id(Integer.parseInt(idTxt.getText())).name(nameTxt.getText()).family(familyTxt.getText()).build();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, member.toString() + " Edited", ButtonType.OK);
             alert.show();
             resetForm();
         });
         removeBtn.setOnAction(event -> {
-            Member member=
-                    Member
-                            .builder()
-                            .id(Integer.parseInt(idTxt.getText()))
-                            .build();
-            Alert alert=new Alert(Alert.AlertType.INFORMATION,
-                    member.toString()+" Removed",
-                    ButtonType.OK);
+            Member member = Member.builder().id(Integer.parseInt(idTxt.getText())).build();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, member.toString() + " Removed", ButtonType.OK);
             alert.show();
         });
     }
 
-    public void resetForm(){
-        try{
+    public void resetForm() {
+        try {
             idTxt.clear();
             nameTxt.clear();
             familyTxt.clear();
             showDataOnTable(MemberController.findAll());
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
 
-    public void showDataOnTable(List<Member> memberList){
+
+    public void showDataOnTable(List<Member> memberList) {
         table.getColumns().clear();
         ObservableList<Member> members = FXCollections.observableList(memberList);
 
@@ -85,6 +71,9 @@ public class MemberViewController implements Initializable {
 
         TableColumn<Member, String> nameCol = new TableColumn<>("Name");
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Member, String> familyCol = new TableColumn<>("Family");
+        familyCol.setCellValueFactory(new PropertyValueFactory<>("family"));
 
 
         table.getColumns().addAll(idCol);
