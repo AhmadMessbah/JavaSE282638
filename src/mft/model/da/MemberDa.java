a0b48ea47bacc3c79d778592fe6ada059c84abc0
royazargar <roya.zargar1992@gmail.com>
2023-11-21 03:30:44 +0330

    member updated royazargar

--- src/mft/model/da/MemberDa.java
+++ src/mft/model/da/MemberDa.java
@@ -4,6 +4,7 @@ import mft.model.entity.Member;
 import mft.model.utils.Jdbc;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,21 +19,30 @@ public class MemberDa implements AutoCloseable{
     public Member save(Member member) throws Exception {
         member.setId(Jdbc.nextId("MEMBER_SEQ"));
         preparedStatement = connection.prepareStatement(
-                "INSERT INTO MEMBER_TBL(ID,NAME,FAMILY) VALUES (?,?,?)"
+                "INSERT INTO MEMBER_TBL(ID,NAME,FAMILY,FATHER,NATIONAL_CODE,BIRTH_DATE,MEMBERSHIP_DATE) VALUES (?,?,?,?,?,?,?)"
         );
         preparedStatement.setInt(1, member.getId());
         preparedStatement.setString(2, member.getName());
         preparedStatement.setString(3, member.getFamily());
+        preparedStatement.setString(4, member.getFather());
+        preparedStatement.setString(5, member.getNationalCode());
+        preparedStatement.setDate(6, Date.valueOf(member.getBirthDate()));
+        preparedStatement.setDate(7 , Date.valueOf(member.getMemberShipDate()));
         preparedStatement.execute();
         return member;
     }
+
     public Member edit(Member member) throws Exception {
         preparedStatement = connection.prepareStatement(
-                "UPDATE MEMBER_TBL SET NAME=?, FAMILY=? WHERE ID=?"
+                "UPDATE MEMBER_TBL SET NAME=?, FAMILY=?, FATHER=? , NATIONAL_CODE=?, BIRTH_DATE=?, MEMBERSHIP_DATE=? WHERE ID=?"
         );
         preparedStatement.setString(1, member.getName());
         preparedStatement.setString(2, member.getFamily());
-        preparedStatement.setInt(3, member.getId());
+        preparedStatement.setString(3, member.getFather());
+        preparedStatement.setString(4, member.getNationalCode());
+        preparedStatement.setDate(5,  Date.valueOf(member.getBirthDate()));
+        preparedStatement.setDate(6 , Date.valueOf(member.getMemberShipDate()));
+        preparedStatement.setInt(7 , member.getId());
         preparedStatement.execute();
         return member;
     }
@@ -46,7 +56,7 @@ public class MemberDa implements AutoCloseable{
 //    }
 
 //         todo : الگو
-public void remove(int id) throws Exception {
+    public void remove(int id) throws Exception {
         preparedStatement = connection.prepareStatement(
                 "UPDATE MEMBER_TBL SET DELETED=1 WHERE ID=?"
         );
@@ -65,6 +75,10 @@ public void remove(int id) throws Exception {
                     .id(resultSet.getInt("ID"))
                     .name(resultSet.getString("NAME"))
                     .family(resultSet.getString("FAMILY"))
+                    .father(resultSet.getString("FATHER"))
+                    .nationalCode(resultSet.getString("NATIONAL_CODE"))
+                    .birthDate(resultSet.getDate("BIRTH_DATE").toLocalDate())
+                    .memberShipDate(resultSet.getDate("MEMBERSHIP_DATE").toLocalDate())
                     .build();
             memberList.add(member);
         }
@@ -83,6 +97,10 @@ public void remove(int id) throws Exception {
                     .id(resultSet.getInt("ID"))
                     .name(resultSet.getString("NAME"))
                     .family(resultSet.getString("FAMILY"))
+                    .father(resultSet.getString("FATHER"))
+                    .nationalCode(resultSet.getString("NATIONAL_CODE"))
+                    .birthDate(resultSet.getDate("BIRTH_DATE").toLocalDate())
+                    .memberShipDate(resultSet.getDate("MEMBERSHIP_DATE").toLocalDate())
                     .build();
         }
         return member;
@@ -101,6 +119,52 @@ public void remove(int id) throws Exception {
                     .id(resultSet.getInt("ID"))
                     .name(resultSet.getString("NAME"))
                     .family(resultSet.getString("FAMILY"))
+                    .father(resultSet.getString("FATHER"))
+                    .nationalCode(resultSet.getString("NATIONAL_CODE"))
+                    .birthDate(resultSet.getDate("BIRTH_DATE").toLocalDate())
+                    .memberShipDate(resultSet.getDate("MEMBERSHIP_DATE").toLocalDate())
+                    .build();
+        }
+        return member;
+    }
+
+    public Member findByNationalCode(String nationalCode) throws Exception {
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM MEMBER_TBL WHERE NATIONAL_CODE=?"
+        );
+        preparedStatement.setString(1, nationalCode);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        Member member = null;
+        while (resultSet.next()) {
+            member = Member.builder()
+                    .id(resultSet.getInt("ID"))
+                    .name(resultSet.getString("NAME"))
+                    .family(resultSet.getString("FAMILY"))
+                    .father(resultSet.getString("FATHER"))
+                    .nationalCode(resultSet.getString("NATIONAL_CODE"))
+                    .birthDate(resultSet.getDate("BIRTH_DATE").toLocalDate())
+                    .memberShipDate(resultSet.getDate("MEMBERSHIP_DATE").toLocalDate())
+                    .build();
+        }
+        return member;
+    }
+
+    public Member findByMemberShipDate(Date memberShipDate) throws Exception {
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM MEMBER_TBL WHERE MEMBERSHIP_DATE=?"
+        );
+        preparedStatement.setDate(1, memberShipDate);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        Member member = null;
+        while (resultSet.next()) {
+            member = Member.builder()
+                    .id(resultSet.getInt("ID"))
+                    .name(resultSet.getString("NAME"))
+                    .family(resultSet.getString("FAMILY"))
+                    .father(resultSet.getString("FATHER"))
+                    .nationalCode(resultSet.getString("NATIONAL_CODE"))
+                    .birthDate(resultSet.getDate("BIRTH_DATE").toLocalDate())
+                    .memberShipDate(resultSet.getDate("MEMBERSHIP_DATE").toLocalDate())
                     .build();
         }
         return member;
